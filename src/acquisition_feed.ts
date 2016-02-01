ea52a513ca353902ef04c24711d5c4f6987d8cf8
Matthew Skomarovsky <matthew@verypolite.com>
2016-02-01 15:35:32 -0500

    FeedParser parses feed's id and updated tags

--- src/acquisition_feed.ts
+++ src/acquisition_feed.ts
@@ -1,9 +1,9 @@
-import OPDSFeed from "./opds_feed";
+import OPDSFeed, { OPDSFeedArgs } from "./opds_feed";
 import OPDSEntry from "./opds_entry";
 import OPDSLink from "./opds_link";
 
 export default class AcquisitionFeed extends OPDSFeed {
-  constructor(title: string, entries: Array<OPDSEntry>, links: Array<OPDSLink>) {
-    super(title, entries, links);
+  constructor(args: OPDSFeedArgs) {
+    super(args);
   }
 }
--- src/opds_feed.ts
+++ src/opds_feed.ts
@@ -2,12 +2,16 @@ import OPDSEntry from "./opds_entry";
 import OPDSLink from "./opds_link";
 
 export default class OPDSFeed {
+  id: string;
   title: string;
+  updated: string;
   entries: Array<OPDSEntry>;
   links: Array<OPDSLink>;
-  constructor(title: string, entries: Array<OPDSEntry>, links: Array<OPDSLink>) {
-    this.title = title;
-    this.entries = entries;
-    this.links = links;
+
+  constructor(args: OPDSFeedArgs) {
+    Object.assign(this, args);
   }
+}
+
+export interface OPDSFeedArgs extends OPDSFeed {
 }
