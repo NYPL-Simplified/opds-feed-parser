f1c39383319edb6539cfc7096b3d95659ec61914
Amy Slagle <amyslagle@nypl.org>
2016-02-02 18:17:32 -0500

    Export additional classes in index so consumers can check them.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,47 @@
 import OPDSParser from "./opds_parser";
+
+import OPDSFeed from "./opds_feed";
+import NavigationFeed from "./navigation_feed";
+import AcquisitionFeed from "./acquisition_feed";
+
+import OPDSEntry from "./opds_entry";
+import PartialOPDSEntry from "./partial_opds_entry";
+
+import Contributor from "./contributor";
+import Category from "./category";
+import Summary from "./summary";
+
+import OPDSLink from "./opds_link";
+import OPDSCatalogRootLink from "./opds_catalog_root_link";
+import OPDSFacetLink from "./opds_facet_link";
+import SearchLink from "./search_link";
+import AlternateLink from "./alternate_link";
+
+import OPDSAcquisitionLink from "./opds_acquisition_link";
+import OPDSPrice from "./opds_price";
+import OPDSArtworkLink from "./opds_artwork_link";
+
 export default OPDSParser;
+
+export {
+  OPDSFeed,
+  NavigationFeed,
+  AcquisitionFeed,
+
+  OPDSEntry,
+  PartialOPDSEntry,
+
+  Contributor,
+  Category,
+  Summary,
+
+  OPDSLink,
+  OPDSCatalogRootLink,
+  OPDSFacetLink,
+  SearchLink,
+  AlternateLink,
+
+  OPDSAcquisitionLink,
+  OPDSPrice,
+  OPDSArtworkLink
+};
