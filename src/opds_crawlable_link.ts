defacdda11f6837449be8c5a89f859eddbfc7e3a
Matthew Skomarovsky <matthew@verypolite.com>
2016-02-03 11:31:11 -0500

    fixed typo in crawlable link rel

--- src/opds_crawlable_link.ts
+++ src/opds_crawlable_link.ts
@@ -1,7 +1,7 @@
 import OPDSLink, { OPDSLinkArgs } from "./opds_link";
 
 export default class OPDSCrawlableLink extends OPDSLink {
-  static REL = "http://opds-spec.org/crawlwable";
+  static REL = "http://opds-spec.org/crawlable";
   constructor(args: OPDSLinkArgs) {
     args.rel = OPDSCrawlableLink.REL;
     super(args);
