c11f1e14cea72ab3e9267b6a4449b6baa0ddf27c
Amy Slagle <amyslagle@nypl.org>
2016-02-04 12:16:15 -0500

    Added tests that use feeds from the OPDS test catalog.

--- test/opds_parser_test.ts
+++ test/opds_parser_test.ts
@@ -1,9 +1,16 @@
 ///<reference path="../typings/main/ambient/mocha/mocha.d.ts" />
 ///<reference path="../typings/main/ambient/chai/chai.d.ts" />
 import OPDSFeed from "../src/opds_feed";
-import NavigationFeed from "../src/navigation_feed";
-import AcquisitionFeed from "../src/acquisition_feed";
+import {
+  NavigationFeed,
+  AcquisitionFeed,
+  OPDSCatalogRootLink,
+  SearchLink,
+  OPDSAcquisitionLink,
+  OPDSArtworkLink
+} from "../src/index";
 import OPDSParser from "../src/opds_parser";
+import fs = require("fs");
 import chai = require("chai");
 let expect = chai.expect;
 
@@ -40,52 +47,74 @@ describe("OPDSParser", () => {
       });
     });
 
-    it("extracts catalog root", (done) => {
-      // http://opds-spec.org/specs/opds-catalog-1-1-20110627/#OPDS_Catalog_Root
-      let opds = "<feed xmlns:atom=\"http://www.w3.org/2005/Atom\"><atom:link href=\"test\" rel=\"start\" /></feed>";
-
-      let promise: Promise<OPDSFeed> = parser.parse(opds);
-      promise.then((result) => {
-        expect(result.links.length).to.equals(1);
-        expect(result.links[0].href).to.equals("test");
-        expect(result.links[0].rel).to.equals("start");
-        done();
-      }).catch((error) => {
-        done(error);
-      });
-    });
-
-    it("recognizes navigation feed", (done) => {
-      // http://opds-spec.org/specs/opds-catalog-1-1-20110627/#Navigation_Feeds
-      let opds = "<feed xmlns:atom=\"http://www.w3.org/2005/Atom\"><atom:entry><atom:link href=\"test\" rel=\"start\" /></atom:entry></feed>";
-      let promise: Promise<NavigationFeed> = parser.parse(opds);
-      promise.then((result) => {
-        if (result) {
-          expect(result instanceof NavigationFeed).to.be.true;
-          done();
+    it("parses navigation feed from OPDS test catalog", (done) => {
+      fs.readFile("test/files/navigation.xml", "utf8", (error, data) => {
+        if (error) {
+          done(error);
         } else {
-          done("parser did not return feed");
+          let promise: Promise<OPDSFeed> = parser.parse(data);
+          promise.then((result) => {
+            expect(result).to.be.an.instanceof(NavigationFeed);
+            expect(result).not.to.be.an.instanceof(AcquisitionFeed);
+            expect(result.id).to.equals("root.xml");
+            expect(result.title).to.equals("Test Catalog Root");
+            expect(result.updated).to.equals("2012-10-20T01:11:18Z");
+
+            expect(result.links.length).to.equals(5);
+            expect(result.links[0].href).to.equals("root.xml");
+            expect(result.links[1]).to.be.an.instanceof(OPDSCatalogRootLink);
+            expect(result.links[2]).to.be.an.instanceof(SearchLink);
+
+            expect(result.entries.length).to.equals(6);
+            expect(result.entries[0].title).to.equals("First Acquisition feed");
+            expect(result.entries[0].id).to.equals("main.xml");
+            expect(result.entries[0].updated).to.equals("2012-10-20T01:11:18Z");
+            expect(result.entries[0].summary.content).to.equals("Basic acquisition feed");
+            expect(result.entries[0].links.length).to.equals(1);
+            expect(result.entries[0].links[0].type).to.equals("application/atom+xml; profile=opds-catalog; kind=acquisition");
+            expect(result.entries[3].title).to.equals("Link: Recommended");
+            expect(result.entries[5].id).to.equals("popular.xml");
+          }).then(done, done);
         }
-      }).catch((error) => {
-        done(error);
       });
     });
 
-    it("recognizes acquisition feed", (done) => {
-      // http://opds-spec.org/specs/opds-catalog-1-1-20110627/#Acquisition_Feeds
-      let opds = "<feed xmlns:atom=\"http://www.w3.org/2005/Atom\"><atom:entry><atom:link href=\"test\" rel=\"http://opds-spec.org/acquisition\"/></atom:entry></feed>";
-      let promise: Promise<AcquisitionFeed> = parser.parse(opds);
-      promise.then((result) => {
-        if (result) {
-          expect(result instanceof AcquisitionFeed).to.be.true;
-          done();
+    it("parses acquisition feed from OPDS test catalog", (done) => {
+      fs.readFile("test/files/acquisition.xml", "utf8", (error, data) => {
+        if (error) {
+          done(error);
         } else {
-          done("parser did not return feed");
+          let promise: Promise<OPDSFeed> = parser.parse(data);
+          promise.then((result) => {
+            expect(result).to.be.an.instanceof(AcquisitionFeed);
+            expect(result).not.to.be.an.instanceof(NavigationFeed);
+            expect(result.id).to.equals("main.xml");
+            expect(result.title).to.equals("First Acquisition Feed");
+            expect(result.updated).to.equals("2012-10-20T01:11:18Z");
+
+            expect(result.links.length).to.equals(4);
+            expect(result.links[1].href).to.equals("../root.xml");
+            expect(result.links[1]).to.be.an.instanceof(OPDSCatalogRootLink);
+            expect(result.links[2]).to.be.an.instanceof(SearchLink);
+
+            expect(result.entries.length).to.equals(17);
+            expect(result.entries[0].title).to.equals("Pride and Prejudice");
+            expect(result.entries[0].id).to.equals("http://feedbooks.github.io/test/1");
+            expect(result.entries[0].authors.length).to.equals(1);
+            expect(result.entries[0].authors[0].name).to.equals("Jane Austen");
+            expect(result.entries[0].updated).to.equals("2012-10-12T17:43:18Z");
+            expect(result.entries[0].summary.content).to.contain("the story of Mrs. Bennet's attempts to marry off her five daughters");
+            expect(result.entries[0].categories.length).to.equals(2);
+            expect(result.entries[0].categories[0].term).to.equals("FBFIC000000");
+            expect(result.entries[0].categories[0].label).to.equals("Fiction");
+
+            expect(result.entries[0].links.length).to.equals(4);
+            expect(result.entries[0].links[0]).to.be.an.instanceof(OPDSAcquisitionLink);
+            expect(result.entries[0].links[0].href).to.equals("http://www.feedbooks.com/book/52.epub");
+            expect(result.entries[0].links[1]).to.be.an.instanceof(OPDSArtworkLink);
+          }).then(done, done);
         }
-      }).catch((error) => {
-        done(error);
       });
     });
-
   });
 });
